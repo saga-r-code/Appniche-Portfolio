c667a825c56f0016808a2228c385c9bdcccb9f00
sagar-workbase-code <shahsagar03@gmail.com>
2024-12-08 17:21:57 +0530

    update

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -48,13 +48,6 @@ const Contact = () => {
       whatsapp: "https://wa.me/+917977528656",
       github: "https://github.com/Redhood-Schizophrenic",
     },
-    {
-      id: 5,
-      username: "Sahil Jaiswal",
-      number: "+91 8419999929",
-      spaciality: "Digital Marketer",
-      whatsapp: "https://wa.me/+918419999929",
-    },
   ];
   return (
     <div id="contact" className="  my-32">
@@ -74,17 +67,14 @@ const Contact = () => {
             <p className="text-slate-400">{item.spaciality}</p>
             <p className="text-slate-400">{item.number}</p>
             <div className="flex justify-center items-center gap-3 mt-5">
-              {item.instagram && (
-                <a href={item.instagram} target="_blank" rel="noreferrer">
-                  <FaSquareInstagram className="text-3xl bg-gradient-to-r from-blue-500 to-blue-800 text-black rounded-md shadow-sm shadow-white/40" />
-                </a>
-              )}
+              <a href={item.instagram} target="_blank" rel="noreferrer">
+                <FaSquareInstagram className="text-3xl bg-gradient-to-r from-blue-500 to-blue-800 text-black rounded-md shadow-sm shadow-white/40" />
+              </a>
 
-              {item.whatsapp && (
-                <a href={item.whatsapp} target="_blank" rel="noreferrer">
-                  <FaSquareWhatsapp className="text-3xl bg-gradient-to-r from-blue-500 to-blue-800 text-black rounded-md shadow-sm shadow-white/40 " />
-                </a>
-              )}
+              <a href={item.whatsapp} target="_blank" rel="noreferrer">
+                <FaSquareWhatsapp className="text-3xl bg-gradient-to-r from-blue-500 to-blue-800 text-black rounded-md shadow-sm shadow-white/40 " />
+              </a>
+
               {item.github && (
                 <a href={item.github} target="_blank" rel="noreferrer">
                   <FaSquareGithub className="text-3xl bg-gradient-to-r from-blue-500 to-blue-800 text-black rounded-md shadow-sm shadow-white/40 " />
